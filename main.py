61033c9298625b2d599a81013f0e6d7bd03caf88
sharanbharadwaj1 <sharanbharadwaj1@gmail.com>
2025-07-12 12:28:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 app = Flask(__name__)
 client = bigquery.Client(project='mlops-ld')
 
+
 @app.route('/')
 def main(big_query_client=client):
     table_id = "mlops-ld.test_schema.us_states"
